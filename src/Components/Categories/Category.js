37014c5424535206ed5b44464909aecf8d776656
Your Name <you@example.com>
2024-12-01 09:53:06 -0800

    created styles for sec

--- src/Components/Categories/Category.js
+++ src/Components/Categories/Category.js
@@ -1,6 +1,10 @@
-import Button from '../Button/Button'
+
 import './Category.css'
-// import img1 from '../../img/img1.jpg'
+import img1 from '../../img/img1.jpg'
+import img2 from '../../img/img2.jpg'
+import img3 from '../../img/img3.jpg'
+import img4 from '../../img/img4.jpg'
+import img5 from '../../img/img5.jpg'
 
   function Category () {
     return(
@@ -10,9 +14,50 @@ import './Category.css'
          <div className='cards flex'>
 
         <div className='card'>
-            <img src={} alt='' className='image'/>           
+            <a  className='link'  href='#' >  
+            
+            <img src={img1} alt='' className='image'/>           
              <p className='txt'></p>
+
+             </a>
         </div>
+
+        <div className='card'>
+            <a  className='link'  href='#' >  
+            
+            <img src={img2} alt='' className='image'/>           
+             <p className='txt'></p>
+
+             </a>
+        </div>
+
+        <div className='card'>
+            <a  className='link'  href='#' >  
+            
+            <img src={img3} alt='' className='image'/>           
+             <p className='txt'></p>
+
+             </a>
+        </div>
+
+        <div className='card'>
+            <a  className='link'  href='#' >  
+            
+            <img src={img4} alt='' className='image'/>           
+             <p className='txt'></p>
+
+             </a>
+        </div>
+
+        <div className='card'>
+            <a  className='link'  href='#' >  
+            
+            <img src={img5} alt='' className='image'/>           
+             <p className='txt'></p>
+
+             </a>
+        </div>
+
          </div>
 
          </div>
