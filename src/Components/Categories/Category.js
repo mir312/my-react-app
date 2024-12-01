3d34f843494608d05d304f770debaa3fe8342e65
Your Name <you@example.com>
2024-12-01 07:49:26 -0800

    write import export

---
+++ src/Components/Categories/Category.js
@@ -0,0 +1,11 @@
+import Button from '../Button/Button'
+import './Category.css'
+// import img1 from '../../img/img1.jpg'
+
+  function Category () {
+    return(
+        <></>
+    )
+}
+
+export default Category
